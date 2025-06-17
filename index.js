bf58c333896e864e881f16a1900dd698eda81701
Elie Soueid <eliegebransoued@gmail.com>
2025-06-17 13:14:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "images"), {
+    maxAge: "30d", // Cache for 30 days
+    setHeaders: (res) => {
+      res.set("Cache-Control", "public, max-age=2592000"); // 30 days
+    },
+  })
+);
+
+app.listen(6000, () => console.log("Server running on http://localhost:6000"));
