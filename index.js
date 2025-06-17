a512ceabc42cdfd3bed625fa665c5a69b566a725
Elie Soueid <eliegebransoued@gmail.com>
2025-06-17 13:53:56 +0200

    correct path

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from "express";
-import path from "path";
 
 const app = express();
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 app.use(
   "/images",
-  express.static(path.join(__dirname, "images"), {
-    maxAge: "30d", // Cache for 30 days
-    setHeaders: (res) => {
-      res.set("Cache-Control", "public, max-age=2592000"); // 30 days
-    },
-  })
+  express.static(
+    ("/home/pilex/assets/flexyshop/images",
+    {
+      maxAge: "30d", // Cache for 30 days
+      setHeaders: (res) => {
+        res.set("Cache-Control", "public, max-age=2592000"); // 30 days
+      },
+    })
+  )
 );
 
 app.listen(6000, () => console.log("Server running on http://localhost:6000"));
