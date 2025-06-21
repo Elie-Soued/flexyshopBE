332935151de24fc2eada3a9717f8a082ea0f0691
Elie Soueid <eliegebransoued@gmail.com>
2025-06-21 14:09:32 +0200

    add stripe session

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Enable CORS for all origins
 app.use(cors());
+app.use(express.json());
 
 // Serve images from your image directory
 const imagePath = "/home/pilex/assets/flexyshop/images";
@@ -18,6 +23,35 @@ app.use(
   })
 );
 
-app.listen(6000, () => {
-  console.log("Server is running on port 6000");
+app.post("/create-checkout-session", async (req, res) => {
+  const { amount } = req.body;
+
+  const stripeAmount = Math.round(amount * 100);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "eur",
+          product_data: {
+            name: "Invoice payment",
+          },
+          unit_amount: stripeAmount,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://your-site.com/success",
+    cancel_url: "https://your-site.com/cancel", // customized (see below)
+  });
+
+  res.json({ url: session.url });
+});
+
+app.get("/", (req, res) => res.send("hello world"));
+
+app.listen(4242, () => {
+  console.log("Server is running on port 4242");
 });
